bc3d24d649dd1e7facd046469d32d971981a8920
miguelslc <k_miguel7@hotmail.com>
2022-04-06 13:50:23 -0300

    updated variables and stuff

--- ejer.js
+++ ejer.js
@@ -1,27 +1,26 @@
 const arr = [1, 2, 3, 'c4', 4, 'asd', 'zxcz', 4, 6, 8];
-let esNumero = arr.filter(number => !isNaN(number));
-console.log(esNumero);
+const isNumber = arr.filter(number => !isNaN(number));
+console.log(isNumber);
 
 
-let pepe = [{
-    nombre: "anibal", apellido: "perez", edad: 23
+const originalObj = [{
+    name: "anibal", lastName: "perez", age: 23
 }, {
-    nombre: "manuel", apellido: "garcia", edad: 30
+    name: "manuel", lastName: "garcia", age: 30
 }, {
-    nombre: "anabela", apellido: "gutierrez", edad: 15
+    name: "anabela", lastName: "gutierrez", age: 15
 }, {
-    nombre: "emiliano", apellido: "perez", edad: 40
+    name: "emiliano", lastName: "perez", age: 40
 }]
-console.log(pepe);
+console.log(originalObj);
 
-let pepe2 = [...pepe]
-pepe2.push({ nombre: "ana", apellido: "ledesma", edad: 55 })
-pepe2 = pepe2.filter(({ apellido }) => apellido === "ledesma").map(({ nombre }) => { return nombre });
-console.log(pepe2);
+let copyObj = [...originalObj]
+copyObj.push({ name: "ana", lastName: "ledesma", age: 55 })
+copyObj = copyObj.filter(({ lastName }) => lastName === "ledesma").map(({ name }) => { return name });
+console.log(copyObj);
 
-let pepe3 = [];
-pepe3 = [...pepe]
-pepe3.push({ nombre: "ana", apellido: "ledesma", edad: 55 })
-pepe3 = pepe3.filter(({ apellido, edad }) => apellido === "perez" && edad > 25).map(({ nombre }) => { return nombre });
-console.log(pepe3);
-//console.log(pepe)
+let anotherCopyObj = [];
+anotherCopyObj = [...originalObj]
+anotherCopyObj.push({ name: "ana", lastName: "ledesma", age: 55 })
+anotherCopyObj = anotherCopyObj.filter(({ lastName, age }) => lastName === "perez" && age > 25).map(({ name }) => { return name });
+console.log(anotherCopyObj);
